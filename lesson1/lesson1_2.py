bd340ec5c0ecf138f0aed329cd7206a39545d291
tsnv76 <tsnv76@yandex.ru>
2022-02-06 01:34:33 +0700

    Files of lesson1

--- lesson1/lesson1_2.py
+++ lesson1/lesson1_2.py
@@ -1,12 +1,13 @@
-# def word_encoding()
-
-words = ['class', 'function', 'method', 'еее']
-# print(eval("'b' + '\'' + words[1]"))
-for item in words:
+def words_encoding(word):
     try:
-        word = 'b' + "'" + item
-        print(word, type(word))
+        result1 = bytes(word, 'ascii')
+        print(f'Содержимое -> "{result1}", длина строки = {len(result1)},  тип данных -> {type(result1)}\n')
     except Exception as e:
-        print(f'ОШИБКА! Слово "{item}" невозможно записать в байтовом типе: {e}')
-    # print(f'Содержимое -> {'item}, lлина строки = {len(item)},  тип данных -> {type(item)}\n')
+        print(f'ОШИБКА! Слово "{word}" невозможно записать в байтовом типе: {e}')
+
+
+words = ['омлром', 'class', 'function', 'method', 'олывсрфывос']
+for item in words:
+    words_encoding(item)
+
 
--- lesson1/lesson1_3.py
+++ lesson1/lesson1_3.py
@@ -1,8 +1,8 @@
 def words_encoding(word):
     try:
-        print(f'Преобразованное слово: {word.encode(encoding="ascii")}')
-        word.encode(encoding='ascii')
-
+        result1 = bytes(word, 'ascii')
+        print(f'Содержимое -> "{result1}"')
+        return  result1
     except Exception as e:
         print(f'ОШИБКА! Слово "{word}" невозможно записать в байтовом типе: {e}')
 
