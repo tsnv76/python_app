73f72fa6601965b0c158e698695d3c2452be0d66
tsnv76 <78307608+tsnv76@users.noreply.github.com>
2022-02-07 10:42:20 +0700

    Update lesson1_2.py

--- lesson1/lesson1_2.py
+++ lesson1/lesson1_2.py
@@ -1,13 +1,17 @@
 def words_encoding(word):
+    st = "'" + word + "'"
     try:
-        result1 = bytes(word, 'ascii')
+        result1 = eval('b' + st)
         print(f'Содержимое -> "{result1}", длина строки = {len(result1)},  тип данных -> {type(result1)}\n')
+        return result1
     except Exception as e:
         print(f'ОШИБКА! Слово "{word}" невозможно записать в байтовом типе: {e}')
+    
 
 
 words = ['омлром', 'class', 'function', 'method', 'олывсрфывос']
 for item in words:
     words_encoding(item)
+
 
 
