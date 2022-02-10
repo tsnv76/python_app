27f3288f0475bd35c918550cb70b49e86ce4f140
tsnv76 <tsnv76@yandex.ru>
2022-02-10 22:14:55 +0700

    Files of lesson2

--- lesson2/lesson2_3.py
+++ lesson2/lesson2_3.py
@@ -7,7 +7,7 @@ def write_dict_to_yaml(dict, file):
         yaml.dump(dict, f_n, default_flow_style=False, allow_unicode=True)
 
     with open(file, encoding='utf-8') as f_n:
-        f_n_content = yaml.load(f_n, Loader=SafeLoader)
+        f_n_content = yaml.load(f_n, Loader=yaml.FullLoader)
 
     print(f_n_content == dict)
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         '1500€': [1, 2, 3, 4],
         '2000€': 8000,
         '30000€': {
-            'first': [1,2,3,4],
+            'first': [1, 2, 3, 4],
             'second': 800,
         }
     }
